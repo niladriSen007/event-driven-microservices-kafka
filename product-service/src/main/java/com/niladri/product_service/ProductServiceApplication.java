ee2a47a47106af586228ab3085eb4bdbe969b6c9
Niladri <niladrisengit12@gmail.com>
2026-03-05 01:33:20 +0530

    feat: introduce product-service application with Kafka and PostgreSQL configuration, and remove Kafka volume from docker-compose.

--- product-service/src/main/java/com/niladri/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/niladri/product_service/ProductServiceApplication.java
@@ -1,5 +1,7 @@
 package com.niladri.product_service;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -9,6 +11,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
