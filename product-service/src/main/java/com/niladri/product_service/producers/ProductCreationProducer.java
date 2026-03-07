7fe1152cc801f582efaeef3df7f5ec42a9dd83fe
Niladri <niladrisengit12@gmail.com>
2026-03-08 02:48:47 +0530

    feat: Add Kafka producer for product creation events along with Kafka and database configuration.

--- product-service/src/main/java/com/niladri/product_service/producers/ProductCreationProducer.java
+++ product-service/src/main/java/com/niladri/product_service/producers/ProductCreationProducer.java
@@ -1,9 +1,6 @@
 package com.niladri.product_service.producers;
 
-import java.util.concurrent.CompletableFuture;
-
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import com.niladri.product_service.events.ProductCreationEvent;
